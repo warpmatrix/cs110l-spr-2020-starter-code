28b701db5c21ea963630b3585998f9e37d8091a9
warpmatrix <warpmatrixlyk@gmail.com>
2025-05-02 16:00:35 +0800

    fin week3 assignment inspect-fds

--- week3/inspect-fds/src/process.rs
+++ week3/inspect-fds/src/process.rs
@@ -1,6 +1,6 @@
 use crate::open_file::OpenFile;
-#[allow(unused)] // TODO: delete this line for Milestone 3
 use std::fs;
+use crate::ps_utils;
 
 #[derive(Debug, Clone, PartialEq)]
 pub struct Process {
@@ -10,7 +10,6 @@ pub struct Process {
 }
 
 impl Process {
-    #[allow(unused)] // TODO: delete this line for Milestone 1
     pub fn new(pid: usize, ppid: usize, command: String) -> Process {
         Process { pid, ppid, command }
     }
@@ -20,22 +19,63 @@ impl Process {
     /// information will commonly be unavailable if the process has exited. (Zombie processes
     /// still have a pid, but their resources have already been freed, including the file
     /// descriptor table.)
-    #[allow(unused)] // TODO: delete this line for Milestone 3
     pub fn list_fds(&self) -> Option<Vec<usize>> {
-        // TODO: implement for Milestone 3
-        unimplemented!();
+        let fd_path = format!("/proc/{}/fd", self.pid);
+        let entries = fs::read_dir(fd_path).ok()?;
+        let mut fds = vec![];
+        for entry in entries {
+            let entry = entry.ok()?;
+            let fd_str = entry.file_name().into_string().ok()?;
+            let fd = fd_str.parse::<usize>().ok()?;
+            fds.push(fd);
+        }
+        Some(fds)
     }
 
     /// This function returns a list of (fdnumber, OpenFile) tuples, if file descriptor
     /// information is available (it returns None otherwise). The information is commonly
     /// unavailable if the process has already exited.
-    #[allow(unused)] // TODO: delete this line for Milestone 4
     pub fn list_open_files(&self) -> Option<Vec<(usize, OpenFile)>> {
         let mut open_files = vec![];
         for fd in self.list_fds()? {
             open_files.push((fd, OpenFile::from_fd(self.pid, fd)?));
         }
         Some(open_files)
+    }
+
+    pub fn print(&self) {
+        println!(
+            "========== \"{}\" (pid {}, ppid {}) ==========",
+            self.command, self.pid, self.ppid
+        );
+        match self.list_open_files() {
+            None => println!(
+                "Warning: could not inspect file descriptors for this process! \
+            It might have exited just as we were about to look at its fd table, \
+            or it might have exited a while ago and is waiting for the parent \
+            to reap it."
+            ),
+            Some(open_files) => {
+                for (fd, file) in open_files {
+                    println!(
+                        "{:<4} {:<15} cursor: {:<4} {}",
+                        fd,
+                        format!("({})", file.access_mode),
+                        file.cursor,
+                        file.colorized_name(),
+                    );
+                }
+            }
+        }
+
+        println!();
+        let child_processes = ps_utils::get_child_processes(self.pid).expect(&format!(
+            "Get process {}'s child processes failed",
+            self.pid
+        ));
+        for child_process in child_processes {
+            child_process.print();
+        }
     }
 }
 
